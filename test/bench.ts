bf89578c6dd3b7fd8e63cf249f32a6ec66648f2f
Fajar Abdi Nugraha <fajar.nugraha@xtremax.com>
2024-10-19 19:19:22 +0700

    dev v0.1.0

---
+++ test/bench.ts
@@ -0,0 +1,28 @@
+import * as ulid from "../mod.ts";
+
+const prng = ulid.detectPrng();
+const uliddValue = ulid.ulid();
+
+Deno.bench("encodeTime", function () {
+    ulid.encodeTime(1469918176385);
+});
+
+Deno.bench("decodeTime", function () {
+    ulid.decodeTime(uliddValue);
+});
+
+Deno.bench("encodeRandom", function () {
+    ulid.encodeRandom(10, prng);
+});
+
+Deno.bench("generate", function () {
+    ulid.ulid(1469918176385);
+});
+
+Deno.bench("ulidToUUID", function () {
+    ulid.ulidToUUID(uliddValue);
+});
+
+Deno.bench("uuidToULID", function () {
+    ulid.uuidToULID(crypto.randomUUID());
+});
