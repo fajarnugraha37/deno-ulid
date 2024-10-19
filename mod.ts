58832d13a39d1e17464af9f051c42cdea431e64f
Fajar Abdi Nugraha <fajar.nugraha@xtremax.com>
2024-10-19 21:56:09 +0700

    add jsdoc to mod file

--- mod.ts
+++ mod.ts
@@ -1,2 +1,41 @@
+/**
+ * Utilities for generating and working with Universally Unique Lexicographically Sortable Identifiers (ULIDs).
+ *
+ * To generate a ULID use the {@linkcode ulid} function. This will generate a
+ * ULID based on the current time.
+ *
+ * ```ts no-assert
+ * import { ulid } from "@fajar/deno-ulid";
+ *
+ * ulid();
+ * ```
+ *
+ * {@linkcode ulid} does not guarantee that the ULIDs will be strictly
+ * increasing for the same current time. If you need to guarantee that the ULIDs
+ * will be strictly increasing, even for the same current time, use the
+ * {@linkcode monotonicUlid} function.
+ *
+ * ```ts no-assert
+ * import { monotonicUlid } from "@fajar/deno-ulid";
+ *
+ * monotonicUlid(); // 01HYFKHG5F8RHM2PM3D7NSTDAS
+ * monotonicUlid(); // 01HYFKHG5F8RHM2PM3D7NSTDAT
+ * ```
+ *
+ * Because each ULID encodes the time it was generated, you can extract the
+ * timestamp from a ULID using the {@linkcode decodeTime} function.
+ *
+ * ```ts
+ * import { decodeTime, ulid } from "@fajar/deno-ulid";
+ * import { assertEquals } from "@std/assert";
+ *
+ * const timestamp = 150_000;
+ * const ulidString = ulid(timestamp);
+ *
+ * assertEquals(decodeTime(ulidString), timestamp);
+ * ```
+ *
+ * @module
+ */
 export * from "./lib/index.ts";
 export type * from "./types/index.d.ts";
